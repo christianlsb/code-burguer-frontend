eb21316013136a93e8f90c52ead71571216bcce1
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2023-02-07 14:40:12 -0300

    first commit

---
+++ src/containers/Login/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import * as S from "./styles";
+
+export default function Login() {
+  return (
+    <S.Container>
+      <S.Background />
+      <S.ContainerItens>
+        <img />
+        <h1>Login</h1>
+        <S.P>Email</S.P>
+        <S.Input />
+        <S.P>Senha</S.P>
+        <S.Input />
+        <Button>Entrar</Button>
+        <S.SingInLink>
+          Não possui conta ? <a>SingUp</a>
+        </S.SingInLink>
+      </S.ContainerItens>
+    </S.Container>
+  );
+}
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+`
+
+export const Background = styled.img`
+
+`
+
+export const P = styled.p`
+
+`
+export const Input = styled.input`
+
+`
+
+export const SingInLink = styled.p`
+
+`
+
+export const ContainerItens = styled.div`
+
+`
