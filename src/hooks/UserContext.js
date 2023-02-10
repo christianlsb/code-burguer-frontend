6f6ececb9ce49dc3b2299d89626260dd7093de98
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2023-02-10 17:17:09 -0300

    feat: added useContext in app

---
+++ src/hooks/UserContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState } from "react";
+import PropTypes from "prop-types";
+
+const UserContext = createContext({});
+
+export const UserProvider = ({ children }) => {
+  const [userData, setUserData] = useState({})
+
+    const putUserData = async (userInfo) => {
+    setUserData(userInfo)
+
+    await localStorage.setItem('codeburguer:userData', JSON.stringify(userInfo))
+
+  }
+
+  return (
+    <UserContext.Provider value={{ putUserData, userData }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (!context) {
+    throw new Error("useUser must be used with UserContext");
+  }
+
+  return context;
+};
+
+UserProvider.propTypes = {
+  children: PropTypes.node,
+};
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
-import SingUp from "./containers/SignUp";
+import Login from "./containers/Login";
 import GlobalStyle from "./styles/globalStyle";
 import { themes } from "./styles/theme";
+import { UserProvider } from "./hooks/UserContext";
 
-import { ToastContainer } from 'react-toastify';
-
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <ThemeProvider theme={themes}>
-      <SingUp />
-      <GlobalStyle /> 
-      <ToastContainer/>
+      <UserProvider>
+        <Login />
+      </UserProvider>
+      <GlobalStyle />
+      <ToastContainer />
     </ThemeProvider>
   </>
 );
