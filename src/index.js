a441aab97a21142a2512225c4c0dbd35ed8b7e5e
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2023-02-11 11:08:18 -0300

    feat: refactor index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
-import Login from "./containers/Login";
-import GlobalStyle from "./styles/globalStyle";
 import { themes } from "./styles/theme";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/globalStyle";
+
+import Login from "./containers/Login";
+
 import { UserProvider } from "./hooks/UserContext";
 
 import { ToastContainer } from "react-toastify";
