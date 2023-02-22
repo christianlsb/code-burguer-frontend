d03719e76766d2c7218f1abacb01883a3c8a77e2
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2023-02-22 12:48:19 -0300

    feat: styling cart

--- src/containers/Cart/index.js
+++ src/containers/Cart/index.js
@@ -6,7 +6,7 @@ import BackgorundCart from "../../assets/bgCart.svg";
 
 import { useCart } from "../../hooks/CartContext";
 
-import formatCurrency from "../../utils/formatCurrency"
+import formatCurrency from "../../utils/formatCurrency";
 
 export function Cart() {
   const { cartProdcuts } = useCart();
@@ -14,23 +14,25 @@ export function Cart() {
 
   return (
     <>
+      <img src={BackgorundCart} style={{width: "100%"}} />
       <S.Container>
-        <S.BgCart src={BackgorundCart} />
         <S.Header>
+          <p></p>
           <p>Itens</p>
           <p>Preço</p>
           <p>Quantidade</p>
           <p>Total</p>
         </S.Header>
-        {cartProdcuts &&
+        {cartProdcuts && cartProdcuts.length > 0 ?
           cartProdcuts.map((product) => (
             <S.Body key={product.id}>
               <img src={product.url} alt="Img_Prodcut" />
               <p>{product.name}</p>
-              <p>{product.price}</p>
+              <p>{product.formatCurrency}</p>
+              <p>{product.quantity}</p>
               <p>{formatCurrency(product.quantity * product.price)}</p>
             </S.Body>
-          ))}
+          )) : <S.EmptyCard>Cart empty</S.EmptyCard>}
       </S.Container>
     </>
   );
--- src/containers/Cart/styles.js
+++ src/containers/Cart/styles.js
@@ -1,13 +1,38 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background: #E5E5E5;
+  background: #ffffff;
+  box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.03);
+  border-radius: 20px;
+  padding: 10px;
+  width: max-content;
+`;
+
+export const Header = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  padding: 10px;
+  border-bottom: 1px solid #b5b5b5;
+  p {
+    font-size: 16px;
+  }
+`;
+export const Body = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  width: max-content;
+  padding: 10px;
+  grid-gap: 10px 15px;
+  img {
+    border-radius: 10px;
+    width: 120px;
+    height: 120px;
+  }
+  p{
+    font-size: 1em;
+  }
+`;
+
+export const EmptyCard = styled.p`
+
 `
-
-export const BgCart = styled.img`
-  width: 100%;
-`
-
-export const Body = styled.div``
-
-export const Header = styled.div``
